bd6820e2bfbdff7b78accc5ac5c8504f98f92bf7
krmazurb <krmazurb@student.wszib.edu.pl>
2025-03-16 17:34:36 +0100

    Baza3

--- accounts.py
+++ accounts.py
@@ -1,3 +1,4 @@
+import datetime
 from database_one import connection
 
 
@@ -13,24 +14,62 @@ class Account:
         self.id = cursor.fetchval()
         connection.commit()
 
-        print(f'Konto zostało utworzone dla {self.name}, z balansem{self.balance}')
+        print(f'Konto zostało utworzone dla {self.name}, z balansem {self.balance}')
 
-    def deposit(self, amount: float) -> float:
+    def deposit(self, amount: float, auto_commit:bool = True) -> float:
         if amount > 0:
             self.balance += amount
+            connection.execute("UPDATE accounts SET balance = ? WHERE account_id = ?", (self.balance,self.id))
+            connection.execute('INSERT INTO  transactions (account_id, transaction_time, amount) VALUES (?,?,?)',
+                               (self.id, datetime.datetime.now(),amount))
+            if auto_commit:
+                connection.commit()
+
             print(f'Na konto {self.name} zostało dodane {amount} PLN')
         return round(self.balance, 2)
 
-    def withdraw(self, amount: float) -> float:
+    def withdraw(self, amount: float, auto_commit:bool = True) -> float:
         if 0 < amount < self.balance:
+            connection.execute("UPDATE accounts SET balance = ? WHERE account_id = ?", (self.balance, self.id))
+            connection.execute('INSERT INTO  transactions (account_id, transaction_time, amount) VALUES (?,?,?)',
+                               (self.id, datetime.datetime.now(), - amount))
             self.balance -= amount
-            print(f'Z konta{self.name} zostało wypłacone{amount} PLN')
+            if auto_commit:
+                connection.commit()
+
+            print(f'Z konta {self.name} zostało wypłacone {amount} PLN')
+        else:
+            raise ValueError("Nie masz wystarczających  środków na koncie")
+
         return round(self.balance, 2)
 
 
+
+    def send_founds(self, amount: float, account):
+        try:
+            self.withdraw(amount, auto_commit=False)
+            account.withdraw(amount, auto_commit=False)
+            connection.commit()
+        except:
+            connection.rollback()
+            print("Tranzakcja została wycofana")
+# tranzakcje
+    def show_transaction(self):
+        # cursor = connection.execute('SELECT DATETIME AS transaction_time')
+        connection.execute('SELECT DATETIME AS transaction_time')
+        connection.execute('SELECT FLOAT AS amount')
+        print()
+
 if __name__ == '__main__':
-    account = Account('Andrzej')
-    account.deposit(10)
-    account.deposit(0.1)
-    balance = account.withdraw(5)
-    print(balance)
+
+    # account = Account('Andrzej')
+    # account.deposit(10)
+    # account.deposit(0.1)
+    # balance = account.withdraw(5)
+    # print(balance)
+    account_jan =Account('Jan', 10)
+    account_michal = Account('Michał', 10)
+    account_jan.send_founds(7, account_michal)
+
+
+
---
+++ alchemy_core.py
@@ -0,0 +1,187 @@
+import os
+from sqlalchemy import create_engine
+import sqlalchemy
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+database_password = os.environ.get('DATABASE_PASSWORD')  # bierze z pliku haslo
+suszi_login = 'krmazurb'
+server = 'morfeusz.wszib.edu.pl'
+driver = 'ODBC+Driver+17+for+SQL+Server'
+
+# dialect+driver://username:password@host:port/database?dodatkowe_opcje_klucz_wartość
+engine = create_engine(
+    f'mssql+pyodbc://{suszi_login}:{database_password}@{server}/{suszi_login}?driver={driver}&Encrypt=no',
+    echo=False #dzieki temu widzimy co SQLAlchemy robi aby dojsc do wyniku
+)
+
+# connection = engine.connect()
+
+# query = sqlalchemy.text("SELECT * FROM workers WHERE pesel=:filter_pesel")
+# result = connection.execute(query, {"filter_pesel": '111111'})
+# print(result.fetchall())
+
+# connection.close()
+
+
+metadata = sqlalchemy.MetaData()
+
+worker_table = sqlalchemy.Table('workers', metadata,  # deklarujemy jak tabela wyglada
+                                sqlalchemy.Column('pesel', sqlalchemy.String(11), primary_key=True),
+                                sqlalchemy.Column('first_name', sqlalchemy.String(255), nullable=False),
+                                sqlalchemy.Column('last_name', sqlalchemy.String(255), nullable=False),
+                                sqlalchemy.Column('birthday', sqlalchemy.Date, nullable=False),
+                                sqlalchemy.Column('address_id', sqlalchemy.Integer),
+                                )
+
+
+connection = engine.connect()
+
+# query = sqlalchemy.text("SELECT * FROM workers WHERE pesel=:filter_pesel")
+query = sqlalchemy.select(worker_table)
+result = connection.execute(query)
+print(result.fetchall())
+
+# perpared statement?
+expression = worker_table.columns.first_name == 'Andrzej'
+print(type(expression))
+print(expression.compile().params)
+print(worker_table.columns.first_name == 'Andrzej')
+
+query = sqlalchemy.select(
+    worker_table.c.first_name, #c. to column
+    worker_table.c.last_name
+)
+
+#inny sposob zapisania tego co jest na gorze
+# query = sqlalchemy.select(
+#     worker_table.c['first_name', 'last_name']
+# )
+
+result = connection.execute(query)
+print(result.fetchall())
+
+# Limit/Top
+query = sqlalchemy.select(worker_table).limit(2)
+result = connection.execute(query)
+print(result.fetchall())
+
+# print('Andrzej' == 'Janek')
+
+#Sortowanie, też może mieć limit(2)
+query = (sqlalchemy.select(worker_table) \
+         .order_by(worker_table.c.first_name.desc(),
+                   worker_table.c.last_name.desc()
+                   ))
+result = connection.execute(query)
+print(result.fetchall())
+
+#Sortowanie 2
+query = sqlalchemy.select(worker_table) \
+         .order_by(worker_table.c.first_name.desc()) \
+         .order_by(worker_table.c.last_name) \
+         .limit(2) \
+         .offset(1)
+
+result = connection.execute(query)
+print(result.fetchall())
+
+# Filtrowanie
+query = sqlalchemy.select(worker_table) \
+    .where(worker_table.c.pesel == '111')
+result = connection.execute(query)
+print(result.fetchall())
+
+# print(type(query))
+# print(query)
+# print(query.compile().params)
+
+#AND
+query = sqlalchemy.select(worker_table) \
+    .where((worker_table.c.address_id > 1) & (worker_table.c.address_id < 4))
+result = connection.execute(query)
+print(result.fetchall())
+
+#AND 2
+query = sqlalchemy.select(worker_table) \
+    .where(worker_table.c.address_id > 1) \
+    .where(worker_table.c.address_id < 4)
+result = connection.execute(query)
+print(result.fetchall())
+
+#AND 3
+query = sqlalchemy.select(worker_table) \
+    .where(sqlalchemy.and_(worker_table.c.address_id > 1, worker_table.c.address_id < 4))
+result = connection.execute(query)
+print(result.fetchall())
+
+# OR  uzywamy pipe |
+query = sqlalchemy.select(worker_table) \
+    .where((worker_table.c.address_id > 1) | (worker_table.c.address_id < 4))
+result = connection.execute(query)
+print(result.fetchall())
+
+#OR 2
+query = sqlalchemy.select(worker_table) \
+    .where(sqlalchemy.or_(worker_table.c.address_id > 1, worker_table.c.address_id < 4))
+result = connection.execute(query)
+print(result.fetchall())
+
+#imie Andrzej lub Martyna i Adres Id > 2
+query = sqlalchemy.select(worker_table) \
+    .where(
+    (worker_table.c.first_name == 'Andrzej') |
+    (worker_table.c.first_name == 'Martyna') &
+    (worker_table.c.address_id > 1)
+)
+result = connection.execute(query)
+print(result.fetchall())
+
+# query = sqlalchemy.select(worker_table) \
+#     .where(
+#     sqlalchemy.and_(
+#         sqlalchemy.or_(worker_table.c.first_name == 'Andrzej', worker_table.c.first_name == 'Martyna'),
+#         worker_table.c.address_id > 1),
+#     )
+# )
+# result = connection.execute(query)
+# print(result.fetchall())
+
+query = sqlalchemy.select(worker_table) \
+    .where(
+    sqlalchemy.and_(
+        worker_table.c.first_name.in_(['Andrzej', 'Martyna']),
+        worker_table.c.address_id > 1
+    )
+)
+result = connection.execute(query)
+print(result.fetchall())
+
+#LIKE
+query = sqlalchemy.select(worker_table) \
+    .where(
+    sqlalchemy.and_(
+        worker_table.c.first_name.like('And%'),
+
+    )
+)
+result = connection.execute(query)
+print(result.fetchall())
+
+query = sqlalchemy.select(sqlalchemy.func.count()).select_from(worker_table)
+result = connection.execute(query)
+print(result.scalar())
+
+query = sqlalchemy.select(sqlalchemy.func.min(worker_table.c.birthday))
+result = connection.execute(query)
+print(result.scalar())
+
+query = sqlalchemy.select(sqlalchemy.func.year(worker_table.c.birthday), sqlalchemy.func.count()) \
+    .group_by(sqlalchemy.func.year(worker_table.c.birthday))\
+    .having(sqlalchemy.func.count() > 1)
+result = connection.execute(query)
+print(result.scalar())
+
+connection.close()
